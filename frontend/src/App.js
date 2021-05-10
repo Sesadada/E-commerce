07819b257b5d2601ac7ecf44e3c78832a2eb3ad5
Sesadada <serenaolivieri@riseup.net>
2021-05-10 10:59:35 +0200

    set Images, set screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container} from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen"
+
 
 
 const App = () => {
   return (
-    <>
-<h1>Welcome to proShop</h1>
-    </>
+
+<Router>
+  <Header />
+  <main className='py-3'>
+  <Container>
+   <h1>Welcome to Witch Craft</h1>
+     <HomeScreen />
+  </Container>
+  </main>
+ <Footer />
+</Router>
+
   );
 }
 
