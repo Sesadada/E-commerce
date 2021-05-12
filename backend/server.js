c308aec938a3b4aa14123c45f13be70742a069f5
Sesadada <serenaolivieri@riseup.net>
2021-05-12 17:26:34 +0200

    mongoDB + postman

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv'; //connects to .env variables
-import products from './data/products.js';
+import colors from 'colors';
+
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
+connectDB();
 
 const app = express(); //creates the backend server
 
@@ -9,19 +14,13 @@ app.get('/', (req, res) => {
 	res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-	//establish a route for the products server
-	res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-	//finds the exacts product based on the id
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
 const PORT = process.env.PORT || 5000; //instead of hardcoded port
 
 app.listen(
 	PORT,
-	console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+	console.log(
+		`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+	)
 );
